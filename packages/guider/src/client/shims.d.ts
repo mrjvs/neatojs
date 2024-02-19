5ff607ca25a873e658a8ac02a6d9fd4d42ccb7c9
mrjvs <mistrjvs@gmail.com>
2024-02-19 22:19:57 +0100

    Add site config types
    Co-authored-by: William Oldham <github@binaryoverload.co.uk>

--- packages/guider/src/client/shims.d.ts
+++ packages/guider/src/client/shims.d.ts
@@ -6,6 +6,7 @@ declare module '@neato/guider/loader!?virtual' {
         to?: string;
         icon?: string;
         newTab?: boolean;
+        items?: Record<string, GuiderThemeNavItem>;
       };
 
   export type GuiderThemeLayout = {
@@ -20,3 +21,49 @@ declare module '@neato/guider/loader!?virtual' {
 
   export const theme: GuiderThemeConfig;
 }
+
+type Conf = SiteConf | SiteConf[];
+
+type LayoutSettings = {
+  colours: any[];
+  urls: any;
+  sidebarConf?: SideBarConf;
+  sidebarEnabled?: boolean;
+  topNavEnabled?: boolean;
+  footerEnabled?: boolean;
+};
+
+type SiteConf = {
+  navItems?: Record<string, GuiderThemeNavItem>;
+  layout?: string;
+  layoutSettings?: PartialDeep<LayoutSettings>;
+
+  directories: {
+    title: string;
+    sidebarItems: Record<string, GuiderThemeNavItem>;
+    layout?: string;
+    layoutSetings?: PartialDeep<LayoutSettings>;
+  }[];
+
+  layouts?: {
+    id: string;
+    settings?: PartialDeep<LayoutSettings>;
+  }[];
+};
+
+type MetaConf = {
+  site?: string;
+  directory?: string;
+  layout?: string;
+};
+
+const _a: Conf = {
+  directories: [
+    {
+      sidebarItems: {
+        '/a': 'Page A',
+        '/b': 'Page B',
+      },
+    },
+  ],
+};
