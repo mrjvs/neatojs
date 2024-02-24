980db91f4e5ec37a2275c7115f1be46ec2a51196
mrjvs <mistrjvs@gmail.com>
2024-02-24 21:01:43 +0100

    Fix github component

--- packages/guider/src/client/components/github.tsx
+++ packages/guider/src/client/components/github.tsx
@@ -18,19 +18,23 @@ export function GithubDisplay(props: GithubDisplayProps) {
       className="gd-flex gd-py-2 gd-px-4 -gd-my-2 gd-rounded-lg gd-items-center gd-bg-opacity-0 hover:gd-bg-opacity-100 gd-bg-bgLight gd-duration-75 gd-transition-[background-color,transform] active:gd-scale-105"
     >
       <Icon
-        icon="fab:github"
+        icon="radix-icons:github-logo"
         className="gd-mr-3 gd-text-xl gd-text-textHeading"
       />
       <div className="gd-flex-1 gd-text-sm">
-        <p className="gd-text-textHeading">{name}</p>
-        <p className="gd-flex gd-items-center gd-gap-4">
-          <span className="gd-space-x-1">
-            <Icon icon="far:star" />
-            <span>{stats ? approx(stats.stars) : null}</span>
+        <p className="gd-text-textHeading -gd-mb-1">{name}</p>
+        <p className="gd-flex gd-items-center gd-gap-3">
+          <span className="gd-space-x-1 gd-flex gd-items-center">
+            <Icon icon="mingcute:star-line" />
+            <span className="gd-pt-0.5">
+              {stats ? approx(stats.stars) : null}
+            </span>
           </span>
-          <span className="gd-space-x-1">
-            <Icon icon="fa:code-branch" />
-            <span>{stats ? approx(stats.forks) : null}</span>
+          <span className="gd-space-x-1 gd-inline-flex gd-items-center">
+            <Icon icon="fe:fork" />
+            <span className="gd-pt-0.5">
+              {stats ? approx(stats.forks) : null}
+            </span>
           </span>
         </p>
       </div>
