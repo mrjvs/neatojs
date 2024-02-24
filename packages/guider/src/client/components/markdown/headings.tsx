4c3fd2ed0c4d853ee758e9df0ac5d0092bda18c8
mrjvs <mistrjvs@gmail.com>
2024-02-24 17:52:55 +0100

    Add heading anchor

--- packages/guider/src/client/components/markdown/headings.tsx
+++ packages/guider/src/client/components/markdown/headings.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react';
+import classNames from 'classnames';
 import type { ElementProps } from './types';
 
 export type MarkdownProps = {
@@ -6,24 +7,48 @@ export type MarkdownProps = {
   children?: ReactNode;
 };
 
+const headingClasses = 'gd-relative gd-group';
+
+function HeadingAnchor(props: { attrs: ElementProps }) {
+  if (!props.attrs.id) return null;
+  return (
+    <a
+      href={`#${props.attrs.id}`}
+      className="group-hover:gd-opacity-25 gd-transition-opacity gd-duration-50 gd-select-none gd-absolute gd-left-0 gd-opacity-0 gd-pr-[.3em] -gd-ml-[1em] hover:!gd-opacity-50 gd-font-normal"
+    >
+      #
+    </a>
+  );
+}
+
 export function MarkdownH1(props: MarkdownProps) {
   return (
     <h1
-      className="gd-font-bold gd-text-textHeading gd-text-2xl gd-mb-3"
       {...props.attrs}
+      className={classNames(
+        'gd-font-bold gd-text-textHeading gd-text-2xl gd-mb-3',
+        props.attrs.class,
+        headingClasses,
+      )}
     >
       {props.children}
+      <HeadingAnchor attrs={props.attrs} />
     </h1>
   );
 }
 
 export function MarkdownH2(props: MarkdownProps) {
   return (
-    <h1
-      className="gd-font-bold gd-text-textHeading gd-text-xl gd-mt-10 gd-mb-4"
+    <h2
       {...props.attrs}
+      className={classNames(
+        'gd-font-bold gd-text-textHeading gd-text-xl gd-mt-10 gd-mb-4',
+        props.attrs.class,
+        headingClasses,
+      )}
     >
       {props.children}
-    </h1>
+      <HeadingAnchor attrs={props.attrs} />
+    </h2>
   );
 }
