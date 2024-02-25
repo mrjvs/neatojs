c6ba218f2869206f14990a69d2a2794c83a2187c
mrjvs <mistrjvs@gmail.com>
2024-02-25 15:32:08 +0100

    Fix redirects

--- packages/guider/src/client/page/redirect.ts
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export type CreateRedirectOptions = {
-  to: string;
-};
-
-export function createRedirect(opts: CreateRedirectOptions) {
-  return () => redirect(opts.to);
-}
