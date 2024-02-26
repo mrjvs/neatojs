b9aa0933f5730a312018e5d48da463b1995e921b
mrjvs <mistrjvs@gmail.com>
2024-02-26 21:43:33 +0100

    Style callouts

--- packages/guider/src/client/components/public/callout.tsx
+++ packages/guider/src/client/components/public/callout.tsx
@@ -1,26 +1,85 @@
 import classNames from 'classnames';
 import type { ReactNode } from 'react';
+import { Icon } from '../icon';
 
-export type CalloutTypes = 'warning' | 'info' | 'error' | 'important';
+export type CalloutTypes = 'warning' | 'note' | 'caution' | 'important' | 'tip';
 
-export interface CalloutProps {
-  type?: CalloutTypes;
+export interface SpecificCalloutProps {
   children?: ReactNode;
 }
 
+export interface CalloutProps extends SpecificCalloutProps {
+  type?: CalloutTypes;
+}
+
 const styleMap: Record<CalloutTypes, string> = {
-  error: 'gd-bg-red-400',
-  important: 'gd-bg-pink-400',
-  info: 'gd-bg-blue-400',
-  warning: 'gd-bg-amber-400',
+  caution:
+    'gd-bg-semanticCaution gd-border-semanticCaution gd-text-semanticCautionLighter',
+  important:
+    'gd-bg-semanticImportant gd-border-semanticImportant gd-text-semanticImportantLighter',
+  tip: 'gd-bg-semanticTip gd-border-semanticTip gd-text-semanticTipLighter',
+  note: 'gd-bg-semanticNote gd-border-semanticNote gd-text-semanticNoteLighter',
+  warning:
+    'gd-bg-semanticWarning gd-border-semanticWarning gd-text-semanticWarningLighter',
+};
+
+const iconStyleMap: Record<CalloutTypes, string> = {
+  caution: 'gd-text-semanticCaution',
+  important: 'gd-text-semanticImportant',
+  tip: 'gd-text-semanticTip',
+  note: 'gd-text-semanticNote',
+  warning: 'gd-text-semanticWarning',
+};
+
+const iconMap: Record<CalloutTypes, string> = {
+  caution: 'ph:warning-octagon',
+  important: 'ph:chat-centered-text',
+  tip: 'ph:lightbulb',
+  note: 'ph:info',
+  warning: 'ph:warning',
 };
 
 export function Callout(props: CalloutProps) {
-  const type = props.type ?? 'info';
+  const type = props.type ?? 'note';
   const classes = styleMap[type];
+  const iconClasses = iconStyleMap[type];
   return (
-    <div className={classNames(classes, 'gd-p-2 gd-rounded-md gd-mb-2')}>
-      {props.children}
+    <div
+      className={classNames(
+        classes,
+        'gd-p-5 gd-rounded-md gd-mb-2 gd-bg-opacity-[7%] gd-border gd-border-opacity-50',
+        'gd-flex',
+      )}
+    >
+      <div
+        className={classNames(
+          iconClasses,
+          'gd-text-xl gd-flex items-center gd-mr-3',
+        )}
+      >
+        <Icon icon={iconMap[type]} />
+      </div>
+      <div className="gd-flex-1">{props.children}</div>
     </div>
   );
+}
+
+export function Tip(props: SpecificCalloutProps) {
+  return <Callout type="tip">{props.children}</Callout>;
+}
+
+export function Note(props: SpecificCalloutProps) {
+  return <Callout type="note">{props.children}</Callout>;
+}
+
+export function Important(props: SpecificCalloutProps) {
+  return <Callout type="important">{props.children}</Callout>;
+}
+
+export function Warning(props: SpecificCalloutProps) {
+  return <Callout type="warning">{props.children}</Callout>;
+}
+
+export function Caution(props: SpecificCalloutProps) {
+  return <Callout type="caution">{props.children}</Callout>;
 }
