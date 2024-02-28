bd5be41ccb9ce34bad57ea910e47646c3e46cd48
mrjvs <mistrjvs@gmail.com>
2024-02-28 23:40:02 +0100

    Migrate neat-config docs

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -1,4 +1,4 @@
-import { defineTheme, directory, link } from '@neato/guider/theme';
+import { defineTheme, directory, group, link } from '@neato/guider/theme';
 
 const github = 'mrjvs/neatojs';
 const dropdown = [
@@ -23,6 +23,36 @@ export default defineTheme([
       directory({
         id: 'guider-writing',
         sidebarItems: [link('Getting started', '/docs/guider')],
+      }),
+    ],
+  },
+  {
+    id: 'config',
+    dropdown,
+    github,
+    directories: [
+      directory({
+        id: 'main',
+        sidebarItems: [
+          group('Guide', [
+            link('Why use @neato/config', '/docs/config/guide/why-neat-config'),
+            link('Installation', '/docs/config/guide/installation'),
+            link('Usage', '/docs/config/guide/usage'),
+            link('Basic example', '/docs/config/guide/basic-example'),
+          ]),
+          group('API', [
+            link('Loaders', '/docs/config/api/loaders'),
+            link('Schemas', '/docs/config/api/schemas'),
+            link('Formatting', '/docs/config/api/formatting'),
+            link('Fragments', '/docs/config/api/fragments'),
+            link('Error handling', '/docs/config/api/errors'),
+            link('Utilities', '/docs/config/api/utils'),
+          ]),
+          group('Miscellaneous', [
+            link('Security', '/docs/config/misc/security'),
+            link('Changelog', '/docs/config/misc/changelog'),
+          ]),
+        ],
       }),
     ],
   },
