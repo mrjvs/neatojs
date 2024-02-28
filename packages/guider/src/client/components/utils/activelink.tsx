65b500d67a87354437c9a49c82fd8ffd5764bb62
mrjvs <mistrjvs@gmail.com>
2024-02-28 18:00:02 +0100

    Active dropdown item

--- packages/guider/src/client/components/utils/activelink.tsx
+++ packages/guider/src/client/components/utils/activelink.tsx
@@ -2,45 +2,61 @@ import { useRouter } from 'next/router';
 import type { LinkProps } from 'next/link';
 import Link from 'next/link';
 import type { ReactNode } from 'react';
-import { useState, useEffect, useMemo } from 'react';
+import { useMemo, useCallback } from 'react';
 import classNames from 'classnames';
+
+export function useAreRoutesActive(
+  ops: {
+    as?: string;
+    href: string;
+    exact?: boolean;
+  }[],
+) {
+  const { asPath, isReady } = useRouter();
+
+  const isActiveCheck = useCallback(
+    (href: string, exact?: boolean) => {
+      if (!isReady) {
+        return false;
+      }
+
+      const linkPathname = new URL(href, location.href).pathname;
+      const activePathname = new URL(asPath, location.href).pathname;
+
+      const linkPathArr = linkPathname.split('/').filter(Boolean);
+      const activePathArr = activePathname.split('/').filter(Boolean);
+
+      if (exact) {
+        const exactMatch = linkPathArr.join('/') === activePathArr.join('/');
+        return exactMatch;
+      }
+
+      let matches = true;
+      for (let i = 0; i < linkPathArr.length; i++) {
+        if (linkPathArr[i] !== activePathArr[i]) {
+          matches = false;
+        }
+      }
+
+      return matches;
+    },
+    [asPath, isReady],
+  );
+
+  const actives = useMemo(() => {
+    return ops.map((v) => isActiveCheck(v.href, v.exact));
+  }, [asPath, isActiveCheck]);
+
+  return actives;
+}
 
 export function useIsRouteActive(ops: {
   as?: string;
   href: string;
   exact?: boolean;
 }) {
-  const { asPath, isReady } = useRouter();
-  const [isActive, setIsActive] = useState(false);
-  useEffect(() => {
-    if (!isReady) {
-      setIsActive(false);
-      return;
-    }
-
-    const linkPathname = new URL(ops.href, location.href).pathname;
-    const activePathname = new URL(asPath, location.href).pathname;
-
-    const linkPathArr = linkPathname.split('/').filter(Boolean);
-    const activePathArr = activePathname.split('/').filter(Boolean);
-
-    if (ops.exact) {
-      const exactMatch = linkPathArr.join('/') === activePathArr.join('/');
-      setIsActive(exactMatch);
-      return;
-    }
-
-    let matches = true;
-    for (let i = 0; i < linkPathArr.length; i++) {
-      if (linkPathArr[i] !== activePathArr[i]) {
-        matches = false;
-      }
-    }
-
-    setIsActive(matches);
-  }, [asPath, isReady, ops.href, ops.exact]);
-
-  return isActive;
+  const actives = useAreRoutesActive([ops]);
+  return actives[0];
 }
 
 type ActiveLinkProps = LinkProps & {
--- packages/guider/src/client/partials/header/dropdown.tsx
+++ packages/guider/src/client/partials/header/dropdown.tsx
@@ -1,9 +1,25 @@
-import { Menu } from '@headlessui/react';
-import ActiveLink from '../../components/utils/activelink';
+import { Menu, Transition } from '@headlessui/react';
+import { Fragment, useMemo } from 'react';
+import classNames from 'classnames';
+import ActiveLink, {
+  useAreRoutesActive,
+} from '../../components/utils/activelink';
 import type { DropdownChildren, LinkComponent } from '../../theme/components';
+import { Icon } from '../../components/icon';
 
 function DropdownItem(props: { link: LinkComponent; active?: boolean }) {
-  return <div>{props.link.title}</div>;
+  return (
+    <div
+      className={classNames({
+        'gd-py-2 gd-px-3 gd-flex gd-items-center gd-transition-colors gd-duration-100 hover:gd-bg-bgLight gd-rounded':
+          true,
+        'gd-text-primary': props.active,
+      })}
+    >
+      <div className="gd-flex-1">{props.link.title}</div>
+      {props.active ? <Icon icon="ph:check-bold" /> : null}
+    </div>
+  );
 }
 
 function DropdownLink(props: { link: LinkComponent }) {
@@ -17,21 +33,51 @@ function DropdownLink(props: { link: LinkComponent }) {
 }
 
 export function HeaderDropdown(props: { dropdown: DropdownChildren[] }) {
-  // TODO find active item
-  const activeItem = props.dropdown[0];
+  const actives = useAreRoutesActive(
+    props.dropdown.map((v) => ({ href: v.to, exact: true })),
+  );
+
+  const activeItem = useMemo(() => {
+    const activeIndex = actives.indexOf(true);
+    if (activeIndex === -1) return props.dropdown[0];
+    return props.dropdown[activeIndex];
+  }, [actives, props.dropdown]);
+
   return (
-    <Menu>
+    <Menu as="div" className="gd-relative -gd-ml-2 gd-inline-block">
       <Menu.Button>
-        <DropdownItem link={activeItem} />
+        {({ open }) => (
+          <div
+            className={classNames({
+              'gd-bg-bg gd-border gd-flex gd-items-center gd-transition-colors gd-duration-100 gd-border-bgLightest gd-py-1 gd-rounded-md gd-px-4 hover:gd-bg-bgLight gd-border-opacity-0':
+                true,
+              '!gd-border-opacity-100 gd-text-textHeading': open,
+            })}
+          >
+            <span className="gd-mr-2 gd-pb-0.5">{activeItem.title}</span>
+            <Icon icon="flowbite:chevron-sort-solid" className="gd-text-text" />
+          </div>
+        )}
       </Menu.Button>
-      <Menu.Items>
-        {props.dropdown.map((item, i) => {
-          const key = `--${i}-${item.to}`;
-          if (item.type === 'link')
-            return <DropdownLink key={key} link={item} />;
-          return null;
-        })}
-      </Menu.Items>
+
+      <Transition
+        as={Fragment}
+        enter="gd-transition gd-ease-out gd-duration-100"
+        enterFrom="gd-transform gd-opacity-0 gd-scale-95"
+        enterTo="gd-transform gd-opacity-100 gd-scale-100"
+        leave="gd-transition gd-ease-in gd-duration-75"
+        leaveFrom="gd-transform gd-opacity-100 gd-scale-100"
+        leaveTo="gd-transform gd-opacity-0 gd-scale-95"
+      >
+        <Menu.Items className="gd-absolute gd-p-2 gd-left-0 gd-mt-2 gd-w-72 gd-origin-top-left gd-rounded-md gd-bg-bg gd-border gd-border-bgLightest">
+          {props.dropdown.map((item, i) => {
+            const key = `--${i}-${item.to}`;
+            if (item.type === 'link')
+              return <DropdownLink key={key} link={item} />;
+            return null;
+          })}
+        </Menu.Items>
+      </Transition>
     </Menu>
   );
 }
