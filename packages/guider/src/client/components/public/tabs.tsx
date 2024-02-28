824aa9b95948091e998e3d1d9b0877fdaf8ac0c0
mrjvs <mistrjvs@gmail.com>
2024-02-28 19:05:07 +0100

    Tabs

--- packages/guider/src/client/components/public/tabs.tsx
+++ packages/guider/src/client/components/public/tabs.tsx
@@ -1,5 +1,6 @@
 import { type ReactNode } from 'react';
 import { Tab } from '@headlessui/react';
+import classNames from 'classnames';
 
 export interface TabsProps {
   items: string[];
@@ -19,12 +20,24 @@ export interface TabsComponent {
 const TabsContainer = (props: TabsProps) => {
   return (
     <Tab.Group>
-      <Tab.List>
+      <Tab.List className="gd-border-b gd-border-line gd-pb-0 gd-space-x-3">
         {props.items.map((v) => (
-          <Tab key={v}>{v}</Tab>
+          <Tab key={v} className="focus:gd-outline-none">
+            {({ selected }) => (
+              <div
+                className={classNames({
+                  'gd-inline-block gd-py-2 hover:gd-text-textLight gd-border-transparent gd-border-b -gd-mb-px gd-px-2':
+                    true,
+                  '!gd-text-textHeading !gd-border-primary': selected,
+                })}
+              >
+                {v}
+              </div>
+            )}
+          </Tab>
         ))}
       </Tab.List>
-      <Tab.Panels>{props.children}</Tab.Panels>
+      <Tab.Panels className="gd-mt-4 gd-mb-8">{props.children}</Tab.Panels>
     </Tab.Group>
   );
 };
