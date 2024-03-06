f136b8417de32f0f19e1eb755367426158f62ec5
mrjvs <mistrjvs@gmail.com>
2024-03-06 12:21:38 +0100

    Make docs structure

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -5,29 +5,33 @@ const template = site('template', {
   dropdown: [link('Guider', '/docs/guider'), link('Config', '/docs/config')],
 });
 
+const starLinks = [
+  link('Github', '/docs/guider/docs/a', {
+    style: 'star',
+    icon: 'akar-icons:github-fill',
+  }),
+  link('Discord', '/docs/guider/docs/d', {
+    style: 'star',
+    icon: 'fa6-brands:discord',
+  }),
+  link('Suggest features', '/docs/guider/docs/c', {
+    style: 'star',
+    icon: 'streamline:chat-bubble-typing-oval-solid',
+  }),
+];
+
 export default defineTheme([
   site('guider', {
     extends: [template],
     tabs: [
-      link('Guides', '/docs/guider/docs'),
+      link('Guides', '/docs/guider/guides'),
       link('Writing', '/docs/guider/writing'),
       link('API reference', '/docs/guider/api-reference'),
     ],
     directories: [
       directory('guider-docs', {
         sidebar: [
-          // TODO
-          //  -> SEO + meta tags
-          //  -> register global components
-          //  -> Changing markdown to custom components
-          //  -> Common setups:
-          //      -> Multiple documentation sites
-          //      -> API reference + documentation
-          //      -> Blog posts + documentation
-          //  -> Redirects
-          //  -> landing page
-          //  -> examples
-          //  -> API reference of theme file and _meta.json files
+          ...starLinks,
           group('Getting started', [
             link('Installation', '/docs/guider/docs/test'), // making a new project
             link('Setting up', '/docs/guider/docs'), // basic of configuring a project
@@ -38,13 +42,24 @@ export default defineTheme([
             ]),
           ]),
           group('Configuration', [
-            link('Colors & theme', '/docs/guider/docs'), // background pattern + colors
-            link('Header', '/docs/guider/docs'), // top nav links + dropdown + github repo
-            link('Tabs', '/docs/guider/docs'), // top nav tabs
-            link('Sidebar', '/docs/guider/docs'), // sidebar links
-            link('Footer', '/docs/guider/docs'), // footer
-            link('Changing the parts', '/docs/guider/docs'), // custom partials
-            link('Deep-dive concepts', '/docs/guider/docs'), // Deep dive into directories, sites and more
+            link('Colors & theme', '/docs/guider/docs'),
+            link('Redirects', '/docs/guider/docs'),
+            link('SEO & Meta tags', '/docs/guider/docs'),
+            link('Landing page', '/docs/guider/docs'),
+            link('Navigation', '/docs/guider/docs'), // changing tabs, dropdown, sidebar
+            link.nested('Common setups', [
+              link('Mutliple docs sites', '/docs/guider/docs'),
+              link('API reference + docs', '/docs/guider/docs'),
+              link('Blog posts + docs', '/docs/guider/docs'),
+            ]),
+            link('Examples', '/docs/guider/docs'),
+          ]),
+          group('Advanced', [
+            link('Running multiple sites', '/docs/guider/docs'), // How to use _meta.json and multiple dirs/sites
+            link('Header', '/docs/guider/docs'), // Changing settings about header (just repo)
+            link('Footer', '/docs/guider/docs'), // Changing settings footer
+            link('Customising layout', '/docs/guider/docs'), // Custom partials
+            link('Deep-dive concepts', '/docs/guider/docs'), // Deep dive into directories, sites and layouts
           ]),
           group('Deploying', [
             link('Github Pages', '/docs/guider/docs'), // deploy on github pages
@@ -56,7 +71,7 @@ export default defineTheme([
       }),
       directory('guider-writing', {
         sidebar: [
-          // all markdown basics
+          ...starLinks,
           group('Markdown', [
             link('Making pages', '/docs/guider/docs'), // How to make pages
             link('Basic text', '/docs/guider/docs'), // Text & headings & inlines & links
@@ -64,7 +79,6 @@ export default defineTheme([
             link('Code blocks', '/docs/guider/docs'), // code blocks & npm2yarn
           ]),
 
-          // more specific markdown
           group('Advanced markdown', [
             link('Tables', '/docs/guider/docs'), // Making tables
             link('Quotes', '/docs/guider/docs'), // quotes
@@ -72,13 +86,44 @@ export default defineTheme([
             link('Dividers', '/docs/guider/docs'), // dividers
           ]),
 
-          // Built-in components and making your own
           group('Components', [
             link('Code groups', '/docs/guider/docs'), // <CodeGroup/>
             link('Callouts', '/docs/guider/docs'), // <all tipes of callouts
             link('Tabs', '/docs/guider/docs'), // <Tabs />
             link('Frames', '/docs/guider/docs'), // <Frame />
             link('Custom components', '/docs/guider/docs'), // making custom components
+          ]),
+        ],
+      }),
+      directory('guider-api-ref', {
+        sidebar: [
+          ...starLinks,
+          group('Theme configuration', [
+            link('defineTheme()', '/docs/guider/docs'),
+            link('site()', '/docs/guider/docs'),
+            link('directory()', '/docs/guider/docs'),
+            link('link()', '/docs/guider/docs'),
+            link('group()', '/docs/guider/docs'),
+            link('seperator()', '/docs/guider/docs'),
+            link('component()', '/docs/guider/docs'),
+          ]),
+
+          group('_meta.json', [
+            link('Structure of _meta.json', '/docs/guider/docs'),
+          ]),
+
+          group('Client functions', [
+            link('createRedirect()', '/docs/guider/docs'),
+            link('createNotFoundPage()', '/docs/guider/docs'),
+            link('useGuider()', '/docs/guider/docs'),
+            link('useGuiderPage()', '/docs/guider/docs'),
+          ]),
+
+          group('Theme components', [
+            link('<GuiderHeader/>', '/docs/guider/docs'),
+            link('<GuiderLayout/>', '/docs/guider/docs'),
+            link('<GuiderSidebar/>', '/docs/guider/docs'),
+            link('<GuiderToc/>', '/docs/guider/docs'),
           ]),
         ],
       }),
