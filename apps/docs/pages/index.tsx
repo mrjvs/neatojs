3d12f93c0bbd4c88a1c8486e76978db6f80925d4
mrjvs <mistrjvs@gmail.com>
2024-03-07 20:07:00 +0100

    Rename test text

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -3,7 +3,7 @@ import { GuiderLayout } from '@neato/guider/client';
 export default function LandingPage() {
   return (
     <GuiderLayout meta={{ layout: 'page' }}>
-      <div>Testing</div>
+      <div>Homepage</div>
     </GuiderLayout>
   );
 }
