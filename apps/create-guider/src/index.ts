6d07490889b2e815d7aa418a76034668baddcd83
mrjvs <mistrjvs@gmail.com>
2024-06-08 22:19:18 +0200

    Fix linting

--- apps/create-guider/src/index.ts
+++ apps/create-guider/src/index.ts
@@ -1,13 +1,13 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
 import { cancel, intro, outro, spinner, text } from '@clack/prompts';
+import { copy, rename } from 'fs-extra';
 import { assertCancel } from './utils/prompts';
 import { exists } from './utils/files';
-import fs from "fs-extra";
-import path from "path";
-import { fileURLToPath } from 'url';
 
 const distPath = path.dirname(fileURLToPath(import.meta.url));
-const cliRoot = path.join(distPath, "../");
-const mainTemplateDir = path.join(cliRoot, "templates/main");
+const cliRoot = path.join(distPath, '../');
+const mainTemplateDir = path.join(cliRoot, 'templates/main');
 
 const s = spinner();
 intro(`@neato/create-guider`);
@@ -17,19 +17,22 @@ const projectName = await text({
   placeholder: 'my-docs',
   validate(value) {
     if (!/\.?[A-Za-z0-9 _-]+/g.test(value))
-      return "Cannot contain special characters";
+      return 'Cannot contain special characters';
   },
 });
 assertCancel(projectName);
 
-const projectDir = "./" + projectName.toString()
+const projectDir = `./${projectName.toString()}`;
 if (await exists(projectDir)) {
-  cancel("A file or folder with that name already exists");
+  cancel('A file or folder with that name already exists');
 }
 
 s.start('Creating files');
-await fs.copy(mainTemplateDir, projectDir);
-await fs.rename(path.join(projectDir, "_gitignore"), path.join(projectDir, ".gitignore"));
-s.stop('Created files')
+await copy(mainTemplateDir, projectDir);
+await rename(
+  path.join(projectDir, '_gitignore'),
+  path.join(projectDir, '.gitignore'),
+);
+s.stop('Created files');
 
 outro(`Enjoy your new documentation!`);
